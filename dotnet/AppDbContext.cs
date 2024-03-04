e9dcb9e6d6376886baf0250603ebba3641ac8ffc
Aleš <foxxcz@gmail.com>
2024-03-04 22:49:47 +0100

    add dotnet

--- dotnet/AppDbContext.cs
+++ dotnet/AppDbContext.cs
@@ -2,7 +2,7 @@ using Microsoft.EntityFrameworkCore;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
 
     public DbSet<Entry> Entries { get; set; }
 }
