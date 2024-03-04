76869634b53c566ea0f6b9b8ff609673188d45e6
Aleš <foxxcz@gmail.com>
2024-03-04 17:02:30 +0100

    output count

--- nodejs/src/app.ts
+++ nodejs/src/app.ts
@@ -2,19 +2,20 @@ import express from 'express';
 import { connectDB } from './db';
 
 const app = express();
-const port = 3000;
 
 app.use(express.json());
 
-app.post('/add-entry', async (req, res) => {
+app.post('/', async (req, res) => {
   const client = await connectDB();
   const { data } = req.body;
-  
+
   await client.query(`INSERT INTO entries(data) VALUES ($1)`, [data]);
-  
-  res.status(201).send({ message: 'Entry added successfully.' });
+
+  const result = await client.query(`SELECT COUNT(*) FROM entries;`);
+  const count = result.rows[0].count;
+
+  res.status(201).send({ message: 'Entry added successfully.', count: count });
   await client.end();
 });
 
 export default app;
-
