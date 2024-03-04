672258850e111abcd7d801c0d9433572445cf28f
Aleš <foxxcz@gmail.com>
2024-03-04 17:06:23 +0100

    app

--- nodejs/src/app.ts
+++ nodejs/src/app.ts
@@ -1,21 +1,24 @@
 import express from 'express';
 import { connectDB } from './db';
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
 
-app.use(express.json());
-
-app.post('/', async (req, res) => {
+app.get('/', async (req, res) => {
   const client = await connectDB();
-  const { data } = req.body;
+  const data = uuidv4();
 
   await client.query(`INSERT INTO entries(data) VALUES ($1)`, [data]);
 
   const result = await client.query(`SELECT COUNT(*) FROM entries;`);
   const count = result.rows[0].count;
 
-  res.status(201).send({ message: 'Entry added successfully.', count: count });
+  res.status(201).send({ message: 'Entry added successfully with random data.', data: data, count: count });
   await client.end();
+});
+
+app.get('/status', (req, res) => {
+  res.status(200).send({ status: 'UP' });
 });
 
 export default app;
